0db444b1986da615146476080ae3e4f6659666db
oguzhan celik <oguzhancelik2027@gmail.com>
2021-02-23 21:51:59 +0300

    üçüncü gönderim

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        // Method injections
+        public void BasvuruYap(IKrediManager krediManager, List<ILoggerService> loggerServices)
+        {
+            // basvuran bilgilerini değerlendirme
+            //
+           
+            krediManager.Hesapla();
+            foreach (var loggerService in loggerServices)
+            {
+                loggerService.Log();
+            }
+        }
+        public void KrediOnBilgilendiremsiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // interface new lenemez
+            // interface onu implement eden klasın referans numarasını tutabiilr
+            // interfaceler birbirinin alternatifi olan ama kod içerikleri
+            //farklı olan durumlar için kullanılır
+            IKrediManager ihtiyacKrediManager = new IhtıyacKrediManager();           
+            IKrediManager tasitKrediManager = new TasitKrediManager();           
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKrediManager(), new List<ILoggerService>
+            {new DataBaseLoggerService(), new SmsLoggerService() });
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, 
+                tasitKrediManager, konutKrediManager};
+
+           // basvuruManager.KrediOnBilgilendiremsiYap(krediler);
+            
+
+
+
+
+
+        }
+    }
+}
